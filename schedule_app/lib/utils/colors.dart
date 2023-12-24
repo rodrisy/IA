deffbd0528a6cb2b347724746a5bc02fb3178c47
rodrisy <rodrigo.s.yuste@gmail.com>
2023-12-24 18:02:42 +0100

    style indexes
    made styling pages for all colors and textstyles that will be used in the app

---
+++ schedule_app/lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+Color bgColor = Color(0xff1f1f1f);
+Color link = Color(0xff007cff);
+Color white = Color(0xffffffff);
+Color red = Color(0xffFF4949);
+Color orange = Color(0xffFF993A);
+Color yellow = Color(0xffFFD952);
+Color green = Color(0xff72FF66);
+Color aqua = Color(0xff55D9E1);
+Color blue = Color(0xff5866E1);
---
+++ schedule_app/lib/utils/fonts.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+TextStyle appbar = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.white);
+TextStyle weekday = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Colors.white);
+TextStyle dayindex = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+    color: Colors.white);
+TextStyle daylink = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+    color: Colors.white);
+TextStyle time = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.w300,
+    color: Colors.white);
+TextStyle subject = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.white);
+TextStyle classinfo = TextStyle(
+    fontFamily: 'Inter',
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+    color: Colors.white);
