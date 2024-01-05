9d7bd882f70b0be4d480ed9baf69448f07891232
rodrisy <rodrigo.s.yuste@gmail.com>
2024-01-05 22:28:56 +0100

    added spinning animation :D
    idk what i am doing, :-1:

--- schedule_app/lib/widgets/toggle_icon_button.dart
+++ schedule_app/lib/widgets/toggle_icon_button.dart
@@ -5,8 +5,32 @@ class ToggleIconButton extends StatefulWidget {
   _ToggleIconButtonState createState() => _ToggleIconButtonState();
 }
 
-class _ToggleIconButtonState extends State<ToggleIconButton> {
+class _ToggleIconButtonState extends State<ToggleIconButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
   bool isSun = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +38,14 @@ class _ToggleIconButtonState extends State<ToggleIconButton> {
       duration: Duration(milliseconds: 500),
       child: IconButton(
         key: Key(isSun.toString()),
-        icon: AnimatedSwitcher(
-          duration: Duration(milliseconds: 500),
+        icon: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Transform.rotate(
+              angle: _animation.value * 2 * 3.1415926535897932,
+              child: child,
+            );
+          },
           child: isSun
               ? Icon(
                   Icons.wb_sunny,
@@ -23,10 +53,11 @@ class _ToggleIconButtonState extends State<ToggleIconButton> {
                 )
               : Icon(
                   Icons.nightlight_round,
-                  color: Colors.yellow,
+                  color: Colors.blue,
                 ),
         ),
         onPressed: () {
+          _controller.forward(from: 0.0);
           setState(() {
             isSun = !isSun;
           });
